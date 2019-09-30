6cde30fe492dc39b734aecbcd059d232dbf3d6ce
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-01 00:34:30 +0200

    Add method to change cpu-img to cpu-pick

--- src/app.js
+++ src/app.js
@@ -23,7 +23,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayCpuMove = (move) => {
     let cpuPick = `Computer played ${move}`
     document.getElementById('cpu-move').innerHTML = cpuPick
+    changeCpuImg(move)
   }
+
+  const changeCpuImg = (path) => {
+    let cpuImage = document.getElementById('cpu_choice').innerHTML
+    console.log(path)
+    switch (path) {
+      case 'Rock':
+        document.getElementById('cpu_choice').innerHTML = '<img src="https://github.com/shsteindorsson/rps_challenge/blob/master/src/img/rock.png?raw=true" alt="CPU-image" id="cpu_img">'
+        break
+      case 'Paper':
+        document.getElementById('cpu_choice').innerHTML = '<img src="https://github.com/shsteindorsson/rps_challenge/blob/master/src/img/paper.png?raw=true" alt="CPU-image" id="cpu_img">'
+        break
+      case 'Scissors':
+        document.getElementById('cpu_choice').innerHTML = '<img src="https://github.com/shsteindorsson/rps_challenge/blob/master/src/img/scissors.png?raw=true" alt="CPU-image" id="cpu_img">'
+        break
+      default:
+        throw `${path} image is not defined`
+        break
+    }
+  }
+  
 
   function cpuMove() {
     let randomNumber = Math.floor(Math.random()*3)
