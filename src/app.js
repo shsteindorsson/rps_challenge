ca4955c9e58f2de95d642d67d80d894cbda53277
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-30 22:27:36 +0200

    Add event listeners for player's choise of move
    Add divs for game messages to 'index.html'.
    Add styling for game messages.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-console.log('Þetta virkar!')
+document.addEventListener('DOMContentLoaded', () => {
+  
+  document.getElementById('rock').addEventListener('click',function(){
+    //alert("clicked rock")
+    displayPlayerMove('Rock')
+  })
+
+  document.getElementById('paper').addEventListener('click',function(){
+    //alert("clicked paper")
+    displayPlayerMove('Paper')
+  })
+
+  document.getElementById('scissors').addEventListener('click',function(){
+    //alert("clicked scissors")
+    displayPlayerMove('Scissors')
+  })
+
+  const displayPlayerMove = (move) => {
+    //alert(move)
+    let playerMove = `You played ${move}!`
+    document.getElementById('player-move').innerHTML = playerMove
+  }
+
+})
