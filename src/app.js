a01ffaca9c02913131a956b38fa457d0b7c708b5
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-30 23:27:34 +0200

    Add functions to pick computers move and write to DOM

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
   
   document.getElementById('rock').addEventListener('click',function(){
-    //alert("clicked rock")
     displayPlayerMove('Rock')
+    displayCpuMove(cpuMove())
   })
 
   document.getElementById('paper').addEventListener('click',function(){
-    //alert("clicked paper")
     displayPlayerMove('Paper')
+    displayCpuMove(cpuMove())
   })
 
   document.getElementById('scissors').addEventListener('click',function(){
-    //alert("clicked scissors")
     displayPlayerMove('Scissors')
+    displayCpuMove(cpuMove())
   })
 
   const displayPlayerMove = (move) => {
-    //alert(move)
     let playerMove = `You played ${move}!`
     document.getElementById('player-move').innerHTML = playerMove
+  }
+
+  const displayCpuMove = (move) => {
+    let cpuPick = `Computer played ${move}`
+    document.getElementById('cpu-move').innerHTML = cpuPick
+  }
+
+  function cpuMove() {
+    let randomNumber = Math.floor(Math.random()*3)
+    switch (randomNumber) {
+      case 0:
+        return 'Rock'
+        break
+      case 1:
+        return 'Paper'
+        break
+      case 2:
+        return 'Scissors'
+        break
+      default:
+        throw `${randomNumber} as a computer move is not defined`
+        break
+    }
   }
 
 })
